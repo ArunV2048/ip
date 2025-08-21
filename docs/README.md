578de2075e9c5b5075b35a1d0b61a44473bb4eb2
ArunV2048 <e1471231@u.nus.edu>
2025-08-22 03:08:07 +0800

    Level-0 : Add chatbot skeleton with greeting and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Yuri User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Yuri.java
@@ -0,0 +1,7 @@
+public class Yuri {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Yuri");
+        System.out.println("What can I do for you?");
+        System.out.println("\nBye. Hope to see you again soon!");
+    }
+}
